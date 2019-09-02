504782fe63745d4a4168200301be4971f404b5cd
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-09-02 20:27:46 +0200

    docs for the functions

--- src/TreeStructure.jl
+++ src/TreeStructure.jl
@@ -347,3 +347,48 @@ function plotD(newtree::Tree)
       #grid(true)
     end
 end
+
+#Documentation for the differernt functions in the script
+                                                                        
+"""
+	Tree()
+Returns a tree with the name,parents of nodes, children of the parents, states of the nodes and probabilities of transition.
+Note that it takes a vector of integers
+Fox example, Tree([1,2,3,4])
+                                                                        
+"""
+
+"""
+	stage()
+Returns the stage of each node in the tree.
+                                                                        
+"""
+
+"""
+	height()
+Returns the height of the tree
+                                                                        
+"""
+
+"""
+	leaves()
+Returns the leaves of the tree and their unconditional probabilities
+                                                                        
+"""
+
+"""
+	nodes()
+Returns the nodes of the tree
+                                                                        
+"""
+
+"""
+	root()
+Returns the root (parent of all nodes) of the tree.
+It also returns a sequence of nodes to reach a specific node.
+For example;
+root(d,5)
+Returns a sequence of nodes to reach node 5 in tree d
+                                                                        
+"""
+                                                                        
