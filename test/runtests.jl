22f0e1de5d8e5116e8230b1748915cd80f2652e8
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-09-08 20:58:55 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,6 @@ using Test
     @testset "ScenTree.jl - Lattice Approximation" begin
         tstLat = LatticeApproximation([1,2,3],GaussianSamplePath,500000,1)
         @test length(tstLat.state) == length(tstLat.probability)
-        @test [sum(tstLat.probability[i]) for i=1:length(tstLat.probability)] == Bool[1,1,1] #sum of probs at every stage
+        @test round.([sum(tstLat.probability[i]) for i=1:length(tstLat.probability)], digits = 1)  == Bool[1,1,1] #sum of probs at every stage
     end
 end
