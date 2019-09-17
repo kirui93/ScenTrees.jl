055c1512afe78633620f388ae911a408df246402
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-09-17 10:11:00 +0200

    remove the dimension on the function

--- src/LatticeApprox.jl
+++ src/LatticeApprox.jl
@@ -10,18 +10,20 @@ mutable struct Lattice
 end
 
 """
-	LatticeApproximation(states::Array{Int64,1},path::Function,nScenarios::Int64,dim::Int64)
+LatticeApproximation(states::Array{Int64,1},path::Function,nScenarios::Int64)
 
-Returns an approximated lattice according to the samples from the `path` provided to. The `dim` variable is the 
-dimension of the states that you are working on. The function `path` generates one sample from a known distribution with length
-equal to the length of states of the lattice and this function takes only two arguments : the number of stages and the dimension.
+Returns an approximated lattice according to the samples from the `path` provided to. The function `path` generates one sample from a known distribution with length
+equal to the length of states of the lattice.
 """
 
-function LatticeApproximation(states::Array{Int64,1},path::Function,nScenarios::Int64,dim::Int64)
+function LatticeApproximation(states::Array{Int64,1},path::Function,nScenarios::Int64)
     WassersteinDistance = 0.0
     rWasserstein = 2
+	
+    dim = size(path(length(states),1),2)
+	
     lns = length(states)
-    LatState = [zeros(states[j],dim) for j = 1:lns]                                                                          # States of the lattice at each time t
+    LatState = [zeros(states[j],dim) for j = 1:lns]                                            # States of the lattice at each time t
     LatProb = vcat([zeros(states[1],1)],[zeros(states[j-1],states[j]) for j = 2:lns])          # Probabilities of the lattice at each time t
 
     #Stochastic approximation
@@ -33,17 +35,17 @@ function LatticeApproximation(states::Array{Int64,1},path::Function,nScenarios::
             sumLat = sum(LatProb[t],dims = 2)
             sqStates = 1.3 * sqrt(n) / states[t]
             tmp = Int64[id for (id,ls) in enumerate(sumLat) if ls < sqStates]
-            LatState[t][tmp] .= Z[t]                                                            #corrective action to include lost nodes
-            mindist,indx = findmin(vec(abs.(LatState[t] .- Z[t])))                              #find the closest lattice entry
-            dist = dist + mindist^2                                                             #Euclidean distance for the paths
-            LatProb[t][idtm1,indx] = LatProb[t][idtm1,indx] .+ 1.0                              #increase the probability
+            LatState[t][tmp] .= Z[t]                                                            # corrective action to include lost nodes
+            mindist,indx = findmin(vec(abs.(LatState[t] .- Z[t])))                              # find the closest lattice entry
+            dist = dist + mindist^2                                                             # Euclidean distance for the paths
+            LatProb[t][idtm1,indx] = LatProb[t][idtm1,indx] .+ 1.0                              # increase the probability
             idtm1 = indx
             LatState[t][indx] = LatState[t][indx] - 2/ (3000 + n)^0.75*rWasserstein*mindist^(rWasserstein-1)*(LatState[t][indx] - Z[t])
         end
         dist = dist^(1/2)
         WassersteinDistance = (WassersteinDistance*(n-1) + dist^rWasserstein)/n
     end                                             
-    LatProb = LatProb ./ nScenarios						#scale the probabilities to 1.0
+    LatProb = LatProb ./ nScenarios						                # scale the probabilities to 1.0
     return Lattice("Lattice Approximation of $states, \n distance=$(round(WassersteinDistance^(1/rWasserstein),digits = 4)) at $(nScenarios) scenarios",LatState,LatProb)
 end
 
