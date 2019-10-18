c5d3be1d77bf714e71437edb9275b7e81b370018
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-18 14:05:00 +0200

    Add Kernel Estimation Method

--- src/ScenTrees.jl
+++ src/ScenTrees.jl
@@ -3,8 +3,9 @@ include("TreeStructure.jl")
 include("TreeApprox.jl")
 include("StochPaths.jl")
 include("LatticeApprox.jl")
+include("KernelDensityEstimation.jl")
 include("bushinessNesDistance.jl")
 export TreeApproximation!,LatticeApproximation,Tree,Lattice,stage,height,leaves,nodes,root,
         partTree,buildProb!,treeplot,plotD,PlotLattice,GaussianSamplePath1D,GaussianSamplePath2D,
-        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance
+        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance,LogisticKernel,KernelScenarios
 end
