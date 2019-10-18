6be386ddb3359976c345a5604aa6a7397e19baf1
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-18 14:13:32 +0200

    Add some examples of data
    This data can be used in the Kernel density estimation method to generate new data conditioned on this provided.

---
+++ src/SomeData.jl
@@ -0,0 +1,17 @@
+# Some examples of data that we can use in the Kernel Density Estimation method
+
+using CSV, DataFrames, XLSX
+
+data = CSV.read(".../data/data5.csv")
+gsData = Matrix(data)
+
+df = DataFrame(XLSX.readtable(".../data/Mappe1.xlsx", "Sheet1")...)
+df1 = Matrix(df)
+df1 = convert(Array{Float64,2},df1)
+
+df2 = DataFrame(XLSX.readtable(".../data/Mappe1.xlsx", "Sheet2")...)
+df22 = Matrix(df2)
+df22 = convert(Array{Float64,2},df22)
+
+RandomWalkData = CSV.read(".../data/RandomDataWalk.csv")
+RWData = Matrix(RandomWalkData)
