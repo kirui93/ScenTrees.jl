2c9ef12e69f8a47543d1ac5bf417cd7074b112e6
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-18 16:00:46 +0200

    Update SomeData.jl

--- src/SomeData.jl
+++ src/SomeData.jl
@@ -2,16 +2,16 @@
 
 using CSV, DataFrames, XLSX
 
-data = CSV.read("./data/data5.csv")
+data = CSV.read("../data/data5.csv")
 gsData = Matrix(data)
 
-df = DataFrame(XLSX.readtable("./data/Mappe1.xlsx", "Sheet1")...)
+df = DataFrame(XLSX.readtable(".-/data/Mappe1.xlsx", "Sheet1")...)
 df1 = Matrix(df)
 df1 = convert(Array{Float64,2},df1)
 
-df2 = DataFrame(XLSX.readtable("./data/Mappe1.xlsx", "Sheet2")...)
+df2 = DataFrame(XLSX.readtable("../data/Mappe1.xlsx", "Sheet2")...)
 df22 = Matrix(df2)
 df22 = convert(Array{Float64,2},df22)
 
-RandomWalkData = CSV.read("./data/RandomDataWalk.csv")
+RandomWalkData = CSV.read("../data/RandomDataWalk.csv")
 RWData = Matrix(RandomWalkData)
