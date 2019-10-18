65aead5979da378f1e76551110b199f07827d72f
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-18 14:37:21 +0200

    Add data into the module

--- src/ScenTrees.jl
+++ src/ScenTrees.jl
@@ -5,7 +5,9 @@ include("StochPaths.jl")
 include("LatticeApprox.jl")
 include("KernelDensityEstimation.jl")
 include("bushinessNesDistance.jl")
+include("SomeData.jl")
 export TreeApproximation!,LatticeApproximation,Tree,Lattice,stage,height,leaves,nodes,root,
         partTree,buildProb!,treeplot,plotD,PlotLattice,GaussianSamplePath1D,GaussianSamplePath2D,
-        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance,LogisticKernel,KernelScenarios
+        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance,LogisticKernel,KernelScenarios,
+        gsData,df1,RWData
 end
