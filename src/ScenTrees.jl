21aadd6aaee7e06fb45e454752bef1565b441b9d
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-19 11:01:07 +0200

    Edit the module for the deleted data

--- src/ScenTrees.jl
+++ src/ScenTrees.jl
@@ -5,9 +5,7 @@ include("StochPaths.jl")
 include("LatticeApprox.jl")
 include("KernelDensityEstimation.jl")
 include("bushinessNesDistance.jl")
-include("SomeData.jl")
 export TreeApproximation!,LatticeApproximation,Tree,Lattice,stage,height,leaves,nodes,root,
         partTree,buildProb!,treeplot,plotD,PlotLattice,GaussianSamplePath1D,GaussianSamplePath2D,
-        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance,LogisticKernel,KernelScenarios,
-        gsData,df1,df22,RWData
+        RunningMaximum1D,RunningMaximum2D,path,bushinessNesDistance,LogisticKernel,KernelScenarios
 end
