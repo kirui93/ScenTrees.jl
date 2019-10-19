da0ad150dc53240cb2e07a8252b2ad3f50f96a8f
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-19 10:58:59 +0200

    Add tests with data on this folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using ScenTrees
 using Test
 using Statistics: std
+using CSV, DataFrames, XLSX
 
 @testset "ScenTrees.jl" begin
     @testset "A sample of a Scenario Tree 1D" begin
@@ -50,23 +51,35 @@ using Statistics: std
         @test round.(sum.(tstLat.probability), digits = 1)  == [1.0, 1.0, 1.0, 1.0] #sum of probs at every stage
     end
     @testset "ScenTrees.jl - Test Example Data" begin
+        data = CSV.read("data5.csv")
+        gsData = Matrix(data)
+        df = DataFrame(XLSX.readtable("Mappe1.xlsx", "Sheet1")...)
+        df1 = Matrix(df)
+        df1 = convert(Array{Float64,2},df1)
+        df2 = DataFrame(XLSX.readtable("Mappe1.xlsx", "Sheet2")...)
+        df22 = Matrix(df2)
+        df22 = convert(Array{Float64,2},df22)
+        RandomWalkData = CSV.read("RandomDataWalk.csv")
+        RWData = Matrix(RandomWalkData)
+        
         @test size(gsData) ==(100,10)
         @test size(df1) == (1000,7)
         @test size(df22) == (1000,4)
         @test size(RWData) == (1000,5)
-    end
-    @testset "ScenTrees.jl - Test Standard deviations" begin
+   
         sd1 = zeros(5)
         sd2 = zeros(7)
+        
         for t = 1:size(RWData,2)
             sd1[t] = std(RWData[:,t])
         end
+        
         for t = 1:size(df1,2)
             sd2[t] = std(df1[:,t])
         end
         @test (sd1 .< 5) == Bool[true, true, true, true, true]
         @test (sd2 .< 10) == Bool[true, true, true, true, true, true, true]
-    end
+    #end
 #     @testset "ScenTrees.jl - Test Kernel trajectory creation" begin
 #         a = KernelScenarios(gsData)
 #         b = KernelScenarios(df1)
@@ -77,7 +90,7 @@ using Statistics: std
 #         @test length(c()) == length(df22,2)
 #         @test length(d()) == length(RWData,2)
 #     end
-    @testset "ScenTrees.jl - Test Kernel Lattice creation" begin
+    #@testset "ScenTrees.jl - Test Kernel Lattice creation" begin
         LatFromKernel = LatticeApproximation([1,3,4,5,6],KernelScenarios(RWData),100000)
         @test round.(sum.(LatFromKernel.probability),digits=1) == [1.0, 1.0, 1.0, 1.0, 1.0]
         @test length(LatFromKernel.state) == length(LatFromKernel.probability)
