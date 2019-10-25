df671f8bf08dcff9104d60b0509eaa43b72f8e93
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-10-25 14:34:04 +0200

    Use the efective sample size

--- src/KernelDensityEstimation.jl
+++ src/KernelDensityEstimation.jl
@@ -10,21 +10,25 @@ using Distributions, Statistics
     User specifies both data and the distribution of the kernel he/she wants to use. The default kernel is the Logistic kernel.
 """
 
-# We will be suing function closures in this as it allows us to call a function inside a function.
 function KernelScenarios(data::Union{Array{Int64,2},Array{Float64,2}},kernelDistribution = Logistic)
-    N,T = size(data)                                               # Dimensions of the data
-    d = 1
-    hN = Array{Float64,1}(undef,T)
-    for t=1:T
-        hN[t] = std(data[:,t]) / (N^(1/(d +4))) + 1e-5            # hN is the bandwidth of the t column
-    end
-    w = fill(1/N,N)                                               # initialize the weights
-    ξ = Array{Float64,1}(undef,T)                                 # trajectory to be created
+    # We use the function closure here because we want to use this function in the stochastic approximation process.
+    # That is, we wwant to follow the specifications of the TreeApproximation and Latticeapproximation inputs.
+    # The way we have created the stochastic approximation functions is that it takes a function which does not take any inputs.
+    # And so the outer function takes the data and the distribution of the kernel while the inner function takes no inputs.
+    # To obtain the result then, call "KernelScenarios(data,Logistic)()".
     function closure()
+        N,T = size(data)                                                  # Dimensions of the data
+        d = 1
+        w = fill(1.0,N)                                                     # initialize the weights
+        ξ = Array{Float64,1}(undef,T)                                     # trajectory to be created
         for t = 1:T
+            w = w / sum(w)                                                # normalized weights
+            Nt = sum(w)^2 / sum(w.^2)                                     # effective sample size
+            σt = sqrt(sum(w .* ((data[:,t] .- sum(w .* data[:,t])).^2)))  # effective standard devistion
+            ht = σt * Nt^(-1/(d + 4)) + eps()
             # Composition method comes in here
             u = rand(Uniform(0,1))
-            _,jstar = findmin(cumsum(w) .> u * sum(w))
+            _,jstar = findmin(cumsum(w) .< u * sum(w))
             # The cumulative sum of weights leads to a high probability of picking a data path near an observation (Sequin && Pichler 2017)
             ξ[t] = rand(kernelDistribution(data[jstar,t],hN[t]))
             # The new generated value is according to the distribution of density of the current stage and dependent on the history of all the data paths.
@@ -35,10 +39,8 @@ function KernelScenarios(data::Union{Array{Int64,2},Array{Float64,2}},kernelDist
                     # The choice of the kernel does not have any important effect on density estimation (Jones (1990)).
                     w[j] *= pdf(kernelDistribution(data[j,t],hN[t]),ξ[t]) 
                 end
-                w = w ./ sum(w)
             end
         end
-        #ξ[1] = mean(data[:,1])           # Fix a common starting value for each trajectory created. This is the root.
         return ξ                         # The length of ξ is equal to the number stages = T
     end
 end
