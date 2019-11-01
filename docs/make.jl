cb964427802b54d6f6a3131fbf4c6b825c4eb916
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-11-01 15:22:20 +0100

    Doctest = false

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(
 	sitename =  "ScenTrees.jl",
 	authors = "Kipngeno Kirui",
 	clean = true,
-	doctest = true,
+	doctest = false,
 	format = Documenter.HTML(
 		assets = ["exampleTree1.png",
 		          "Tree402.png",
@@ -23,14 +23,14 @@ makedocs(
 		          ],
 		prettyurls = get(ENV, "CI", nothing) == "true"),
 	pages = ["Home" => "index.md",
-			"Tutorials" => Any["tutorial/tutorial1.md",
-			                    "tutorial/tutorial2.md",
-			                    "tutorial/tutorial3.md",
-					    		"tutorial/tutorial31.md",
-			                    "tutorial/tutorial4.md",
-					    		"tutorial/tutorial41.md",
-					    		"tutorial/tutorial5.md"]
+		"Tutorials" => Any["tutorial/tutorial1.md",
+				    "tutorial/tutorial2.md",
+				    "tutorial/tutorial3.md",
+				    "tutorial/tutorial31.md",
+				    "tutorial/tutorial4.md",
+				    "tutorial/tutorial41.md",
+				    "tutorial/tutorial5.md"]
 			                    ]
 )
 
-deploydocs(repo   = "github.com/kirui93/ScenTrees.jl.git")
+deploydocs(repo = "github.com/kirui93/ScenTrees.jl.git")
