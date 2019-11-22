9754bba3da6c60dfc8d9ba7744d36a1a1e38f445
Kipngeno Kirui <36660022+kirui93@users.noreply.github.com>
2019-11-22 15:40:57 +0100

    format and deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@ using Documenter, ScenTrees
 
 #const ASSETS = readdir(joinpath(@__DIR__, "src", "assets"))
 
+isCI = get(ENV, "CI", nothing) == "true" #Travis populates this env variable by default
+
 makedocs(
 	sitename =  "ScenTrees.jl",
 	authors = "Kipngeno Kirui",
 	clean = true,
 	doctest = true,
-	format = Documenter.HTML(
-		#assets = ASSETS,
-		prettyurls = get(ENV, "CI", nothing) == "true"),
+	format = Documenter.HTML(prettyurls = isCI),		
 	pages = ["Home" => "index.md",
 		"Tutorials" => Any["tutorial/tutorial1.md",
 				    "tutorial/tutorial2.md",
@@ -18,10 +18,10 @@ makedocs(
 				    "tutorial/tutorial4.md",
 				    "tutorial/tutorial41.md",
 				    "tutorial/tutorial5.md"
-				]
-			                    
+				] 
 		]
 )
 
-
-deploydocs(repo = "github.com/kirui93/ScenTrees.jl.git")
+if isCI
+    deploydocs(repo = "github.com/kirui93/ScenTrees.jl.git")
+end
